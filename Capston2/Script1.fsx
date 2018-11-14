6f245d8191cfc904ccea2873a2474d467a18e815
Brian Snyder <snitz70@hotmail.com>
2018-11-14 15:18:09 -0600

    working audit function

--- Capston2/Script1.fsx
+++ Capston2/Script1.fsx
@@ -11,11 +11,23 @@ let withdraw amount account =
 let fileSystemAudit account message = 
 
 let console account message =
-    printfn "Account %A: Performed operation %s.  Balance is now $%M" account.Id message account.Balance
+    printfn "Account %O: %O %s " 
+        account.Id System.DateTime.Now message 
 
 let auditAs (operationName:string) (audit:Account -> string -> unit)
     (operation:decimal -> Account -> Account)  (amount:decimal)
     (account:Account) : Account =
+    
+    audit account (sprintf "Beginning balance $%M Performing %s of $%M " account.Balance operationName amount)
+    let newAccount = operation amount account
+
+    let accountChanged = account <> newAccount
+    
+    if accountChanged then audit newAccount (sprintf "Transaction Accepted.  New balance %M" newAccount.Balance)
+    else audit newAccount (sprintf "Transaction rejected")
+
+    newAccount
+
 
 
 let withdrawConsoleAudit = auditAs "withdraw" 
@@ -23,3 +35,8 @@ let customer = { Name = "Brian" }
 let account = { Id = System.Guid.Empty; Owner = customer; Balance = 100M }
 let account2 = account |> withdraw 25M |> deposit 50M
 let con = console account "Withdraw"
+
+let consoleWithdraw = auditAs "withdraw" console withdraw
+let consoleDeposit = auditAs "Deposit" console deposit
+
+account |> consoleWithdraw 25M |> consoleDeposit 50M |> consoleWithdraw 200M
